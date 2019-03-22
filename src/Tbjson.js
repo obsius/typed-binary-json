1314e834ea9490ecbb3e67ea1773c72ed85c4b19
obsius <mr.jeffrey.seaman@gmail.com>
2019-03-22 01:47:31 -0700

    .

--- src/Tbjson.js
+++ src/Tbjson.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 
+// stamp for file type
 const STAMP = '.tbj';
 
-const BYTE =    0;
-const BOOL =    1;
-const UINT =    2;
+// types
+const NULL =    0;
+const BYTE =    1;
+const BOOL =    2;
 const INT8 =    3;
 const UINT8 =   4;
 const INT16 =   5;
@@ -16,12 +18,22 @@ const FLOAT64 = 10;
 const STRING =  11;
 const ARRAY =   12;
 const OBJECT =  13;
+const CUSTOM =  14;
+
+// type sizes
+const SIZE_NULL =    1;
+const SIZE_BYTE =    1;
+const SIZE_BOOL =    1;
+const SIZE_INT8 =    1;
+const SIZE_UINT8 =   1;
+const SIZE_INT16 =   2;
+const SIZE_UINT16 =  2;
+const SIZE_INT32 =   4;
+const SIZE_UINT32 =  4;
+const SIZE_FLOAT32 = 4;
+const SIZE_FLOAT64 = 8;
 
 const CUSTOM_TYPE_OFFSET = 128;
-
-const SIZES = {
-	[FLOAT32]: 4
-};
 
 /**
  * Tbjson
@@ -61,7 +73,7 @@ export default class Tbjson {
 	serialize(stream, obj) {
 		
 		// make a buffer
-		this.buffer = new DynamicBuffer(stream, this.options.bufferSize);
+		this.buffer = new StreamBufferWriter(stream, this.options.bufferSize);
 
 		// write out the stamp
 		this.buffer.write(STRING, STAMP);
@@ -102,17 +114,78 @@ export default class Tbjson {
 
 	/* private */
 
-	formatDef(def) {
+	// TODO diff between ref and def
+	addDef(ref, def) {
+		switch (typeof subDef) {
+			case 'string':
+
+				return [subDef];
+
+			case 'object':
+				if (Array.isArray(subDef)) {
+					return subDef;
+				} else {
+					let 
+					for (let key in def) {
+						let subDef = def[key];
+						return formatDef;
+					}
+				}
+				break;
+
+			case 'number':
+			case 'boolean':
+				// invalid
+				break;
+		}
 
 	}
 
 	processDef(obj, def) {
-		for (let key in def) {
-			this.buffer.write(def[key][0], obj[key]);
+
+		// is a primitive
+		if (typeof def == 'number') {
+			this.buffer.write(def, obj);
+			if (def == STRING) { this.buffer.write(NULL); }
+
+		// is a sub object or array
+		} else {
+
+			// is def or array
+			if (Array.isArray(def)) {
+
+				// is fixed-length variable type array
+				if (Array.isArray(def[0])) {
+					for (let i = 0; i < def.length; ++i) {
+						processDef(obj[i], def[i]);
+					}
+
+				// is variable-length fixed typed array
+				} else if (def[0] == ARRAY) {
+					for (let i = 0; i < obj.length; ++i) {
+						processDef(obj[i], def[0]);
+					}
+					this.buffer.write(NULL);
+
+				// is custom
+				} else if (def[0] == CUSTOM) {
+					this.processDef(obj, this.classes[def[1]]);
+
+				// is def
+				} else {
+					this.processDef(obj, def[0]);
+				}
+
+			// is a sub object
+			} else {
+				for (let key in def) {
+					this.processDef(obj[key], def[key]);
+				}
+			}
 		}
 	}
 
-	process(obj, def) {
+	process(obj) {
 		switch (typeof obj) {
 			case 'boolean':
 				this.buffer.write(BOOL, obj);
@@ -145,7 +218,7 @@ export default class Tbjson {
 						let def = obj.constructor.tbjson.def
 
 						if (!this.classes[ref]) {
-							this.classes[ref] = this.formatDef(obj.constructor.tbjson.def);
+							this.addDef(ref, def);
 						}
 
 						this.processDef(obj, obj.constructor.tbjson.def);
@@ -166,11 +239,13 @@ export default class Tbjson {
 	}
 }
 
+Tbjson.TYPES = { BYTE, BOOL, INT8, UINT8, INT16, UINT16, INT32, UINT32, FLOAT32, FLOAT64, STRING, ARRAY, OBJECT, CUSTOM };
+
 /* internal */
 
-class DynamicBuffer {
+class StreamBufferWriter {
 
-	constructor(stream, size = 1024, xFactor = 2) {
+	constructor(stream, size = 16384, xFactor = 2) {
 
 		this.stream = stream;
 		this.buffer = Buffer.allocUnsafe(size);
@@ -184,7 +259,12 @@ class DynamicBuffer {
 	}
 
 	flush() {
-		this.stream.write(this.buffer.slice(0, this.offset));
+		this.streamReady = false;
+
+		this.stream.write(this.buffer.slice(0, this.offset), (rdy) => {
+			this.streamReady = true;
+		});
+
 		this.offset = 0;
 	}
 
@@ -192,16 +272,32 @@ class DynamicBuffer {
 		this.buffer = Buffer.concat(this.buffer, Buffer.allocUnsafe(this.size * Math.floor(this.xFactor / 2)));
 	}
 
+	checkFlush(length) {
+		if (this.offset + length > this.size) {
+			if (this.streamReady) {
+				this.flush();
+			} else {
+				this.resize();
+			}
+		}
+	}
+
 	write(type, val, length = 0) {
 		switch (type) {
-			case FLOAT32:
-				if (this.offset + SIZES[type] > this.size) {
-					this.flush();
-				}
-				this.buffer.writeFloatLE(val, this.offset);
-				this.offset += 4;
+			case NULL:
+				this.checkFlush(SIZE_NULL);
+				this.buffer.writeByte(0, this.offset++);
+				this.offset += SIZE_NULL;
 				break;
+
+			case FLOAT32:
+				this.checkFlush(SIZE_FLOAT32);
+				this.buffer.writeFloatLE(val, this.offset);
+				this.offset += SIZE_FLOAT32;
+				break;
+
 			case STRING:
+				this.checkFlush(val.length);
 				this.buffer.write(val, this.offset, val.length, 'utf-8');
 				this.offset += val.length;
 		}
@@ -209,9 +305,9 @@ class DynamicBuffer {
 }
 
 
+
+
 /* TESTS */
-
-
 
 let tbjson = new Tbjson();
 
@@ -219,63 +315,56 @@ class A {
 	x = 12234345.343452;
 	y = 15234234.124525;
 	z = 23123231235.14134;
+	zz = {
+		a: 'a',
+		b: 12,
+		c: [-1.2, false]
+	}
 }
 A.tbjson = {
-
+	ref: 'A',
+	def: {
+		x: Tbjson.TYPES.FLOAT32,
+		z: Tbjson.TYPES.FLOAT32,
+		y: Tbjson.TYPES.FLOAT32,
+		zz: {
+			a: Tbjson.TYPES.STRING,
+			b: Tbjson.TYPES.FLOAT32,
+			c: [Tbjson.TYPES.FLOAT32, Tbjson.TYPES.BOOL]
+		}
+	}
 };
 
 class B {
 	constructor() {
+
 		this.as = [];
 		for (let i = 0; i < 1000000; ++i) {
 			this.as.push(new A());
 		}
+
+		this.b = 'b';
 	}
 }
 B.tbjson = {
 	ref: 'B',
 	def: {
-		as: [ARRAY, 'A']	
+		as: [Tbjson.TYPES.ARRAY, 'A']
 	}
 };
-
-/*
-tbjson.registerClass(A, {
-	x: FLOAT32,
-	y: FLOAT32,
-	z: FLOAT32
-});
-
-tbjson.registerClass(B, {
-	as: [ARRAY, A]
-});
-*/
 
 let x = {
 	b: new B(),
 	c: 'test',
-	d: [12, 14, "est"]
-};
-
-/*
-let x = {
-	v: [],
-	s: 'hello',
-	r: {
-		s: 'hello2',
-		d: 45
-	}
+	d: [12, 14, 'est'],
+	e: []
 };
 
 for (let i = 0; i < 10; ++i) {
-	x.v.push(i);
-}*/
+	x.e.push(i);
+}
 
 console.time();
-/*
-for (let i = 0; i < x.v.length; ++i) {
-
-}*/
 
 //fs.writeFileSync("test.kk", JSON.stringify(x));
 
